fc7ae9c4efaca718e6008128ed687765df67889c
saksham duseja <sakshamduseja222@gmail.com>
2019-03-29 21:12:29 -0400

    First commit

---
+++ manjot_march29_2/c0732136/c0732136/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0732136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hw = new HelloWorld();
+            hw.Hello();
+        }
+        class HelloWorld
+        {
+            public void Hello()
+            {
+
+                Console.WriteLine("Hello World");
+
+            }
+
+        }
+
+    }
+}
