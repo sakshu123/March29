8849e62f945ed3ef9436aa5b9b72ae22f242c2bf
saksham duseja <sakshamduseja222@gmail.com>
2019-03-29 20:14:49 -0400

    Second commit

---
+++ sakshu_march_29/C0732116_2/C0732116_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C0732116_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Downloading a file");
+            Download();
+            Console.ReadLine();
+
+
+        }
+
+        public static void Download ()
+        {
+            Thread.Sleep(3000);
+
+        }
+
+    }
+}
